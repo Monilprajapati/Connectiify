b3778568a5afdb8c6c22613d577bbf4c8521e6b7
Umang Dalvadi <umangdalvadi8@gmail.com>
2024-02-10 02:02:54 +0530

    add comment and message model

---
+++ server/src/models/commentModel.js
@@ -0,0 +1,40 @@
+import { Schema, model } from "mongoose";
+
+const commentSchema = new Schema({
+    owner: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ],
+    commentData: String,
+    upvotes: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ],
+    downvotes: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ],
+    subComment: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Comment'
+        }
+    ],
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+    },
+}, {
+    timestamps: true
+});
+
+export const Comment = model("Comment", commentSchema);
