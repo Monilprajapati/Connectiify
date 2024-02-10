6dbc310fd10409a208babe9dcad960ecea0e3790
Umang Dalvadi <umangdalvadi8@gmail.com>
2024-02-10 15:47:57 +0530

    add postCard and post component

--- client/src/components/PostComponents/Post.jsx
+++ client/src/components/PostComponents/Post.jsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import PostCard from "./PostCard";
+import { useNavigate } from "react-router-dom";
+import getPosts from "../../services/posts";
+import { useUserContext } from "../../contexts/userContext";
+import { ThreeCircles } from "react-loader-spinner";
+import { useSelector } from "react-redux";
 
-const Post = () => {
+const Posts = () => {
+  const { token, posts, setPosts, open } = useUserContext();
+  const [loading, setLoading] = useState(false);
+  const isAuthenticated = useSelector((state) => state.isAuthenticated);
+
+  useEffect(() => {
+    if (token) {
+      setLoading(true);
+      getPosts(setPosts, token);
+    }
+    setLoading(false);
+  }, [setPosts, isAuthenticated]);
+
   return (
-    <div>Post</div>
-  )
-}
+    <>
+      {loading ? (
+        <div className="h-full w-full flex justify-center items-center">
+          <ThreeCircles
+            visible={true}
+            height="50"
+            width="50"
+            color="gray"
+            innerCircleColor="gray"
+            middleCircleColor="#E5E5E5"
+            outerCircleColor="#6B6B6B"
+            ariaLabel="three-circles-loading"
+            wrapperStyle={{}}
+            wrapperClass=""
+          />
+        </div>
+      ) : (
+        <div
+          className={`mb-2 py-1 px-2 md:px-11 ${open ? "lg:px-4 xl:px-36" : "lg:px-14 xl:px-40"
+            } flex flex-col gap-5`}
+        >
+          {posts
+            .slice()
+            .reverse()
+            .map((post) => {
+              return (
+                <PostCard
+                  key={post._id}
+                  postId={post._id}
+                  description={post.description}
+                  image={post.image}
+                  upvotes={post.upvotes}
+                  downvotes={post.downvotes}
+                  comments={post.comments}
+                  owner={post.owner}
+                  tag={post.tag}
+                  createdAt={post.createdAt}
+                />
+              );
+            })}
+        </div>
+      )}
+    </>
+  );
+};
 
-export default Post
+export default Posts;
--- client/src/components/PostComponents/PostCard.jsx
+++ client/src/components/PostComponents/PostCard.jsx
@@ -1,9 +1,170 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+// import { posts, user } from "../../common/data";
+import moment from "moment";
+import { BiUpvote, BiDownvote, BiComment } from "react-icons/bi";
+import { useUserContext } from "../../contexts/userContext";
+import updatePost from "../../services/updatePost";
 
-const PostCard = () => {
+const PostCard = ({
+  postId,
+  description,
+  image,
+  upvotes,
+  downvotes,
+  comments,
+  owner,
+  tag,
+  createdAt,
+}) => {
+  const { token, user, allusers } = useUserContext();
+  const [seeMore, setSeeMore] = useState(false);
+  const [upvoteCount, setUpvoteCount] = useState(upvotes.length);
+  const [downvoteCount, setDownvoteCount] = useState(downvotes.length);
+  const [postOwner, setPostOwner] = useState();
+  const [isUpvoteClicked, setIsUpvoteClicked] = useState(
+    upvotes.includes(user?._id)
+  );
+  const [isDownvoteClicked, setIsDownvoteClicked] = useState(
+    downvotes.includes(user?._id)
+  );
+
+
+  const handleUpvote = async () => {
+    try {
+      await updatePost(postId, "upvote", token);
+
+      setIsUpvoteClicked(!isUpvoteClicked);
+      setUpvoteCount((prevCount) =>
+        isUpvoteClicked ? prevCount - 1 : prevCount + 1
+      );
+
+      if (isDownvoteClicked) {
+        setIsDownvoteClicked(false);
+        setDownvoteCount((prevCount) => prevCount - 1);
+      }
+    } catch (error) {
+      console.error("Error upvoting post:", error.message);
+    }
+  };
+
+  const handleDownvote = async () => {
+    try {
+      await updatePost(postId, "downvote", token);
+
+      setIsDownvoteClicked(!isDownvoteClicked);
+      setDownvoteCount((prevCount) =>
+        isDownvoteClicked ? prevCount - 1 : prevCount + 1
+      );
+
+      if (isUpvoteClicked) {
+        setIsUpvoteClicked(false);
+        setUpvoteCount((prevCount) => prevCount - 1);
+      }
+    } catch (error) {
+      console.error("Error downvoting post:", error.message);
+    }
+  };
+
+  useEffect(() => {
+    const user = allusers.find((user) => user._id === owner);
+    setPostOwner(user);
+  }, [allusers, user]);
+
   return (
-    <div>PostCard</div>
-  )
-}
+    <div className=" w-full bg-lightGray px-4 md:px-6 py-6 pb-3 flex flex-col justify-between rounded-md">
+      {/* userInfo */}
+      <div className="flex gap-3 items-center mb-3">
+        <img
+          src={postOwner?.userAvatar}
+          alt={`avatar`}
+          className="w-14 h-14 object-cover border border-white rounded-full"
+        />
+        <div className="w-full flex justify-between">
+          <div className="flex flex-col">
+            <p className="font-medium text-lg text-ascent-1">
+              {postOwner?.username}
+            </p>
+            <span className="text-sm opacity-70">
+              {moment(createdAt).fromNow()}
+            </span>
+          </div>
+        </div>
+      </div>
 
-export default PostCard
+      {tag && (
+        <div className="flex gap-2 items-center mb-3">
+          {tag.map((tag, index) => (
+            // eslint-disable-next-line react/jsx-key
+            <span
+              key={index}
+              className="text-xs xl:text-sm opacity-80 text-black bg-white p-2 rounded-md"
+            >
+              {tag}
+            </span>
+          ))}
+        </div>
+      )}
+      <div className="pt-4 w-full border-t border-black border-opacity-20">
+        {/* <div className="text-md xl:text-lg">
+          {description.length < 401
+            ? description
+            : seeMore
+            ? description
+            : description.slice(0, 400) + "..."}
+          <span
+            className="text-[#00BFFF] ml-2 font-mediu cursor-pointer"
+            onClick={() => setSeeMore(!seeMore)}
+          >
+            {description.length < 271 ? "" : seeMore ? "See Less" : "See More"}
+          </span>
+        </div> */}
+        <span className="w-full">
+          {description}
+        </span>
+
+        {image && (
+          <img
+            src={image}
+            alt="post image"
+            className="w-full mt-4 rounded-lg"
+          />
+        )}
+      </div>
+
+      <div className="mt-4 pt-4 mb-1 flex justify-between items-center px-2 py-2 text-ascent-2 text-base border-t">
+        <div className="flex gap-3 items-center text-base cursor-pointer">
+          <button
+            onClick={handleUpvote}
+            className={`py-1 px-2 w-16 rounded-full flex items-center gap-2 border-2 group hover:border-green ${
+              isUpvoteClicked
+                ? "border-green border-opacity-100"
+                : "border-dark-grey border-opacity-35 "
+            }`}
+          >
+            <BiUpvote size={20} />
+            {upvoteCount}
+          </button>
+
+          <button
+            onClick={handleDownvote}
+            className={`py-1 px-2 w-16 rounded-full flex items-center gap-2 border-2 border-dark-grey group hover:border-red ${
+              isDownvoteClicked
+                ? "border-red border-opacity-100"
+                : "border-dark-grey border-opacity-35"
+            }`}
+          >
+            <BiDownvote size={20} />
+            {Math.abs(downvoteCount)}
+          </button>
+        </div>
+
+        <p className="flex gap-2 items-center text-base cursor-pointer">
+          <BiComment size={20} />
+          {comments.length}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default PostCard;
