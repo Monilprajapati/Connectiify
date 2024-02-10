ecf0ce25327694105f4566311b5c0fe2cd8e7283
monilprajapati <monillprajapatii@gmail.com>
2024-02-10 13:26:43 +0530

    Added Navbar and Completed Dashboard

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { useState } from "react";
+import { HiOutlineMenuAlt2 } from "react-icons/hi";
+import { Outlet } from "react-router-dom";
+import SearchInput from "./SearchInput";
+import { Link } from "react-router-dom";
+import Dropdown from "./Dropdown";
+import { useSelector } from "react-redux";
+import { MdGroups } from "react-icons/md";
+
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
 
-export default Navbar
+  // this state will tell if user is authenticated or not
+  const authStates = useSelector((state) => state.authReducer.isAuthenticated);
+  return (
+    <>
+      <nav className="navbar font-lato font-semibold">
+        <Link className="flex items-center gap-2" to="/">
+          <span>
+            <MdGroups className="text-xl  md:text-2xl lg:text-4xl xl:text-5xl"/>
+          </span>
+          <span className="text-xl  md:text-2xl lg:text-3xl xl:text-4xl">
+            Connectiify
+          </span>
+        </Link>
+        {authStates && (
+          <div className="flex md:hidden">
+            <HiOutlineMenuAlt2
+              size={30}
+              onClick={() => {
+                setOpen(!open);
+              }}
+            />
+          </div>
+        )}
+        {authStates && (
+          <div className="hidden md:flex md:w-2/3 justify-end items-center gap-5">
+          <SearchInput />
+          <Link className="bg-white px-2.5 font-semibold py-2.5 rounded-md" to="/alumniconnect">
+          Alumni Connet
+          </Link>
+          <Dropdown />
+          </div>
+          // <div className="hidden md:flex md:w-full">
+          //   {/* <SearchInput /> */}
+          // </div>
+        )}
+      </nav>
+      <Outlet />
+    </>
+  );
+};
+
+export default Navbar;
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import cf from "../assets/cf.png"
+import { Link } from "react-router-dom";
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
-  )
-}
+    <div className="flex flex-col lg:flex-row h-[calc(100vh-80px)] overflow-hidden gap-6 md:gap-10 -mb-40 w-full items-center justify-center">
+      <div className="px-10 flex flex-col gap-6 mt-10">
+        <h1 className="text-4xl font-lato font-semibold capitalize text-center">
+          Welcome to the Connectiify
+        </h1>
+        <p className="text-lg font-plusSans capitalize text-center px-10">
+        Connectiify is a secure web platform where college students can anonymously connect, share experiences, seek advice, and engage in discussions with peers from the same institution.
+        </p>
+        <div className="buttons mt-2 md:mt-5 font-medium flex gap-4 items-center justify-center">
+            <Link to="/login" className="btn-dark">
+                Login
+            </Link>
+            <Link to="/register" className="btn-light">
+                Register
+            </Link>
+        </div>
+      </div>
+        <img src={cf} alt="cf" className=" md:w-2/3 lg:w-1/2" />
+    </div>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
