f064eadb0111ca4e8d533d88e182c189fb120743
kenil-kanani <kenilkananigecbvn@gmail.com>
2024-02-10 09:29:24 +0530

    Added cloudnary middleware to uplode images

--- server/src/config/serverConfig.js
+++ server/src/config/serverConfig.js
@@ -10,3 +10,7 @@ export const EMAIL_ID = process.env.EMAIL_ID;
 export const EMAIL_PASSWORD = process.env.EMAIL_PASSWORD;
 export const TOKEN_EXPIRY = process.env.TOKEN_EXPIRY;
 export const TOKEN_SECRET = process.env.TOKEN_SECRET;
+export const CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME;
+export const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY;
+export const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET;
+export const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
